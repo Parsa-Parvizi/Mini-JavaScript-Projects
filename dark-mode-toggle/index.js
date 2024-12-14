40481afa6bdfe983bd659a29704e55221850fb65
Parsa Parvizi <138492415+Parsa-Parvizi@users.noreply.github.com>
2024-12-14 03:51:00 +0330

    Add files via upload

---
+++ dark-mode-toggle/index.js
@@ -0,0 +1,28 @@
+/**
+ * The function toggles the background color of the body element between black and white based on the
+ * state of a checkbox input and stores the state in local storage.
+ */
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+  } else {
+    bodyEl.style.background = "white";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
